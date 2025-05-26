457ff283da99e332f6c15dcfb244aace631c95a5
surf3095 <tmwshi123@gmail.com>
2025-05-25 19:23:37 -0500

    Add files via upload

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -5,17 +5,21 @@ from models import db
 from auth import auth
 import os
 
-app = Flask(__name__)
-app.config.from_object("config.Config")
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.Config")
 
-db.init_app(app)
-jwt = JWTManager(app)
+    db.init_app(app)
+    jwt = JWTManager(app)
+    app.register_blueprint(auth)
 
-app.register_blueprint(auth)
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+    return app
+
+app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- .github/workflows/models.py
+++ .github/workflows/models.py
@@ -13,5 +13,5 @@ class User(db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self):
         return check_password_hash(self.password_hash, password)
